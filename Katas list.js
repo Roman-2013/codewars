8c241438b6204831be39a2e8b7ac4e7439741ed4
Roman-2013 <roma.pushkarev.1995@mail.ru>
2023-04-21 23:45:19 +0300

    21.04.2023

--- Katas list.js
+++ Katas list.js
@@ -75,4 +75,34 @@ function helloWorld(){
 console.log(str);
   };
 
+  //Массивы
+
+  function getLength(arr){
+  //return length of arr
+  return arr.length;
+}
+function getFirst(arr){
+  //return the first element of arr
+  return arr[0];
+}
+function getLast(arr){
+  //return the last element of arr
+  return arr[arr.length-1]
+}
+function pushElement(arr){
+  var el=1;
+  //push el to arr
+  arr.push('1');
+  return arr;
+}
+function popElement(arr){
+  //pop an element from arr
+  arr.pop();
+  return arr
+}
+
+//Тип данных-обьект
+function animal(obj){
   
+  return "This "+obj.color+" "+obj.name+" has "+obj.legs+" legs.";
+}
