4195a4bedfcbea6549268d622297a00e9db2a5a4
Roman-2013 <roma.pushkarev.1995@mail.ru>
2023-04-30 22:13:09 +0300

    Update 8 kyu.js

--- 8 kyu.js
+++ 8 kyu.js
@@ -44,3 +44,73 @@ function abbrevName(name){
   function sumMix(x) {
   return x.reduce((acc, curr) => acc + Number(curr), 0);
 }
+
+//Считать овец...
+function countSheeps(arrayOfSheep) {
+  var i=0;
+  for(let a=0;a<arrayOfSheep.length;a++){
+    if(arrayOfSheep[a]){
+      i++;
+    }
+    }
+  return i;
+}
+
+//Транспорт в отпуске
+function rentalCarCost(d) {
+  if(d<3){
+    return d*40;
+  }else if(3 <=d && d< 7){
+    return d * 40 - 20;
+  }else {
+    return d * 40 - 50;
+  }
+}
+
+//
+L1: Установить будильник
+function setAlarm(employed, vacation){
+  if(employed && vacation==false){
+    return true;
+  }else{
+    return false
+  }
+}
+
+//Thinkful - Logic Drills: Traffic light
+function updateLight(current) {
+  if(current=="green"){
+    return "yellow";
+  }else if(current=="yellow"){
+    return "red";
+  }else if(current=="red"){
+    return "green";
+  }
+}
+
+//Вернуть отрицательный результат
+function makeNegative(num) {
+ var a= String(num);
+  if(a[0]==='-'){
+    return Number(a)
+  }return Number(-a);
+  
+  }
+
+  //Побеждает самая высокая прибыль!
+  function minMax(arr){
+  return [Math.min(...arr), Math.max(...arr)];
+}
+
+//Remove exclamation marks
+function removeExclamationMarks(s) {
+  let i=[];
+  for(let a=0;a<s.length;a++){
+    if(s[a]=='!'){
+    delete s[a];
+    }else{
+      i.push(s[a])
+    }
+  }
+  return i.join('');
+}
